a6d327eade4f9f794c0882682dbd58259da2a53b
Gustavo Silva <gustavodev60@gmail.com>
2022-06-23 21:33:17 -0300

    adjust register page

--- lib/modules/register/presentation/register_page.dart
+++ lib/modules/register/presentation/register_page.dart
@@ -19,6 +19,7 @@ class _RegisterPageState extends State<RegisterPage> {
       TextEditingController();
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _phoneController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   bool isChecked = false;
   @override
@@ -34,6 +35,7 @@ class _RegisterPageState extends State<RegisterPage> {
     _passwordController.dispose();
     _confirmPasswordController.dispose();
     _phoneController.dispose();
+
     super.dispose();
   }
 
@@ -59,83 +61,95 @@ class _RegisterPageState extends State<RegisterPage> {
                           height: screenHeightPercentage(context, 0.2)),
                     ),
                   ),
-                  Flexible(
-                    flex: 3,
-                    child: Column(
-                      children: [
-                        CustomTextFormField(
-                          keyboardType: TextInputType.emailAddress,
-                          controller: _emailController,
-                          label: "Email Address",
-                          hint: "Usernames@gmail.com",
-                          prefixIcon: Icon(Icons.email_outlined,
-                              color: Colors.grey[700]),
-                        ),
-                        SizedBox(height: screenHeightPercentage(context, 0.04)),
-                        CustomTextFormField(
-                          keyboardType: TextInputType.name,
-                          controller: _nameController,
-                          label: "Name",
-                          hint: "Enter your name",
-                          prefixIcon: Icon(Icons.person_outline,
-                              color: Colors.grey[700]),
-                        ),
-                        SizedBox(height: screenHeightPercentage(context, 0.04)),
-                        CustomTextFormField(
-                          keyboardType: TextInputType.phone,
-                          controller: _phoneController,
-                          label: "Phone",
-                          hint: "Enter your phone number",
-                          prefixIcon: Icon(Icons.contact_phone_outlined,
-                              color: Colors.grey[700]),
-                        ),
-                        SizedBox(height: screenHeightPercentage(context, 0.04)),
-                        CustomTextFormField(
-                          keyboardType: TextInputType.visiblePassword,
-                          controller: _passwordController,
-                          obscureText: true,
-                          label: "Password",
-                          prefixIcon:
-                              Icon(Icons.lock_outline, color: Colors.grey[700]),
-                          hint: "********",
-                          suffixIcon: const VisibleWidget(),
-                        ),
-                        SizedBox(height: screenHeightPercentage(context, 0.04)),
-                        CustomTextFormField(
-                          keyboardType: TextInputType.visiblePassword,
-                          controller: _confirmPasswordController,
-                          obscureText: true,
-                          label: "Confirm your password",
-                          prefixIcon:
-                              Icon(Icons.lock_outline, color: Colors.grey[700]),
-                          hint: "********",
-                          suffixIcon: const VisibleWidget(),
-                        ),
-                        SizedBox(
-                          height: screenHeightPercentage(context, 0.02),
-                        ),
-                        Row(
-                          children: [
-                            Checkbox(
-                              checkColor: Colors.white,
-                              activeColor: Colors.purple[600],
-                              value: isChecked,
-                              onChanged: (bool? value) {
-                                setState(() {
-                                  isChecked = value!;
-                                });
-                              },
-                            ),
-                            const Text(
-                              "I have read and agree to the terms of use.",
-                              style: TextStyle(
-                                  color: Colors.grey,
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w500),
-                            ),
-                          ],
-                        ),
-                      ],
+                  Form(
+                    key: _formKey,
+                    child: Flexible(
+                      flex: 3,
+                      child: Column(
+                        children: [
+                          CustomTextFormField(
+                            keyboardType: TextInputType.emailAddress,
+                            controller: _emailController,
+                            label: "Email Address",
+                            hint: "Usernames@gmail.com",
+                            prefixIcon: Icon(Icons.email_outlined,
+                                color: Colors.grey[700]),
+                            textInputAction: TextInputAction.next,
+                          ),
+                          SizedBox(
+                              height: screenHeightPercentage(context, 0.04)),
+                          CustomTextFormField(
+                            keyboardType: TextInputType.name,
+                            controller: _nameController,
+                            label: "Name",
+                            hint: "Enter your name",
+                            prefixIcon: Icon(Icons.person_outline,
+                                color: Colors.grey[700]),
+                            textInputAction: TextInputAction.next,
+                          ),
+                          SizedBox(
+                              height: screenHeightPercentage(context, 0.04)),
+                          CustomTextFormField(
+                            keyboardType: TextInputType.phone,
+                            controller: _phoneController,
+                            label: "Phone",
+                            hint: "Enter your phone number",
+                            prefixIcon: Icon(Icons.contact_phone_outlined,
+                                color: Colors.grey[700]),
+                            textInputAction: TextInputAction.next,
+                          ),
+                          SizedBox(
+                              height: screenHeightPercentage(context, 0.04)),
+                          CustomTextFormField(
+                            keyboardType: TextInputType.visiblePassword,
+                            controller: _passwordController,
+                            obscureText: true,
+                            label: "Password",
+                            prefixIcon: Icon(Icons.lock_outline,
+                                color: Colors.grey[700]),
+                            hint: "********",
+                            suffixIcon: const VisibleWidget(),
+                            textInputAction: TextInputAction.next,
+                          ),
+                          SizedBox(
+                              height: screenHeightPercentage(context, 0.04)),
+                          CustomTextFormField(
+                            keyboardType: TextInputType.visiblePassword,
+                            controller: _confirmPasswordController,
+                            obscureText: true,
+                            label: "Confirm your password",
+                            prefixIcon: Icon(Icons.lock_outline,
+                                color: Colors.grey[700]),
+                            hint: "********",
+                            suffixIcon: const VisibleWidget(),
+                            textInputAction: TextInputAction.done,
+                          ),
+                          SizedBox(
+                            height: screenHeightPercentage(context, 0.02),
+                          ),
+                          Row(
+                            children: [
+                              Checkbox(
+                                checkColor: Colors.white,
+                                activeColor: Colors.purple[600],
+                                value: isChecked,
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    isChecked = value!;
+                                  });
+                                },
+                              ),
+                              const Text(
+                                "I have read and agree to the terms of use.",
+                                style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                   Flexible(
